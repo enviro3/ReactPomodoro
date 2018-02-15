9efc15007f05f1b6944f32bd47a6038639bf9862
Caroline Vurlumis <cvurlumis@gmail.com>
2018-02-14 21:21:06 -0800

    remove React countdown clock

--- pomodoro/src/components/PomoTime.js
+++ pomodoro/src/components/PomoTime.js
@@ -1,21 +1,8 @@
 import React, {Component} from 'react';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import dateFormat from 'dateformat';
-import ReactCountdownClock from 'react-countdown-clock';
-
 
 class PomoTime extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      isPaused: true
-    };
-  }
-
-  finishTime(){
-    console.log("Fin!")
-    console.log(ReactCountdownClock)
-  }
 
   secondsToFormattedTime(seconds) {
     console.log("this work?")
@@ -25,54 +12,29 @@ class PomoTime extends Component {
   }
 
   startTimer() {
-    console.log('got here');
-    console.log(ReactCountdownClock);
-
-    console.log(ReactCountdownClock.paused);
+    if (this.timer == 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+  countDown() {
+    let seconds = this.state.seconds - 1;
     this.setState({
-      isPaused: false
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
     });
 
-    //ReactCountdownClock.paused = false;
-    console.log('got here 2');
-
-   // hide Start button
-   /* show Reset Button */
-   // Add the timer to the page using some jquery to grab a destination
-  }
-
-  resetTimer() {
-    this.setState({seconds:5});
-    this.setState({seconds:10});
-   // hide reset button
-   /* show start button */
-   // do something with the timer
-  }
-
+    if (seconds == 0) {
+      clearInterval(this.timer);
+    }
 
   render(){
-    var bob = (<ReactCountdownClock seconds={20}
-                 color="#000"
-                 alpha={0.9}
-                 size={300}
-                 paused={this.state.isPaused}
-                 onComplete={this.finishTime}
-                 seconds={this.state.seconds}
-
-
-    />);
     return (
       <div>
         <div className="PomoTime">
            <li> A clock will go here Timer, clock display, button </li>
         </div>
-        <button id="startButton" onClick={ () => this.startTimer() }>Start</button>
-        <button id="resetButton" onClick={ (/*this.resetTimer*/) => {console.log(this.resetTimer()); console.log(ReactCountdownClock);} }>Reset</button>
-        <h4 id="timerTarget"></h4>
-
+        <button onClick={ (this.startTimer) => console.log("Hello") }>Start</button>
         <h4>{this.secondsToFormattedTime(60 * 25)}</h4>
-
-{bob}
       </div>
 
     );
